e3a2a3825a7917c163466ecdfa19689d4a4d9b13
mania7539 <mania7539@gmail.com>
2018-01-16 13:22:04 +0800

    Initial Commit

---
+++ glfw-xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  glfw-xcode
+//
+//  Created by Liu Ray on 16/01/2018.
+//  Copyright © 2018 Liu Ray. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
